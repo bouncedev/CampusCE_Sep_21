a33b5cbec6ed8d9191131c9553100daacb74ab87
Catalyss <Catalyss@Melindas-iMac.hsd1.tx.comcast.net>
2021-09-21 13:47:33 -0500

    setting up dev environment for Bounce/CampusCE

---
+++ campuslogin.js
@@ -0,0 +1,12 @@
+
+//togglePassword to hide/show password for login
+const togglePassword = document.querySelector('#togglePassword');
+   const password = document.querySelector('#ucLoginForm_password');
+
+   togglePassword.addEventListener('click', function (e) {
+     // toggle the type attribute
+     const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+     password.setAttribute('type', type);
+     // toggle the eye slash icon
+     this.classList.toggle('fa-eye-slash');
+ });
